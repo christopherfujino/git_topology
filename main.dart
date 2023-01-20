64a37e8535443fb52d95704100efa9423d4a27ad
Christopher Fujino <christopherfujino@gmail.com>
2023-01-20 15:44:58 -0800

    add package:process

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
 import 'dart:io' as io;
+import 'package:process/process.dart';
 
-Future<void> main() {}
+Future<void> main() async {
+  final pm = LocalProcessManager();
+  final process = await pm.start(
+    const <String>['git', 'log', '--reflog'],
+    mode: io.ProcessStartMode.inheritStdio,
+  );
+  await process.exitCode;
+}
